8cd2d538dfdc645d3f5490bcdda8d84f616ceb05
Sebastien <perigon@hotmail.com>
2021-01-12 19:16:34 +0000

    refactored as two file one for connection another for the game

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const net = require('net');
-const { on } = require('process');
-
-
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  
-  conn.setEncoding('utf8');
-  conn.on("connect", () => {console.log("Connexion established") }) 
- 
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
