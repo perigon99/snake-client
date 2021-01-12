ff0265d3b0c9b9056d0898d8ba4895cd6e16af11
Sebastien <perigon@hotmail.com>
2021-01-12 20:47:15 +0000

    hard codded mouvement

--- client.js
+++ client.js
@@ -15,8 +15,30 @@ const connect = function() {
   
   conn.setEncoding('utf8');
   conn.on("connect", () => {console.log("Connexion established") }) 
-  conn.on("connect", () => {conn.write("Name: max")})
+  conn.on("connect", () => {
+    conn.write("Name: max");
+    // conn.write("Move: up");
+    // setInterval(() => {
+    //  conn.write("Move: up"); 
+    // }, 150)
+    // setInterval(() => {
+    //   conn.write("Move: left"); 
+    //  }, 160)
+    //  setInterval(() => {
+    //   conn.write("Move: left"); 
+    //  }, 170)
+    //  setInterval(() => {
+    //   conn.write("Move: down"); 
+    //  }, 180)    
+  })
+  
+
   return conn;
 }
 
 module.exports = {connect};
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
