18627587fe6431d597c003c72f118a39d7aad8d8
Sebastien <perigon@hotmail.com>
2021-01-13 00:48:52 +0000

    handleling ctrl+c exit parameter

--- play.js
+++ play.js
@@ -5,3 +5,32 @@ connect();
 console.log('Connecting ...');
 
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(callback) {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on(callback, (key) => {
+    key = key
+    if (key === '\u0003') {
+    process.exit();
+  }
+  
+    return stdin;
+  })
+  
+  
+  
+}
+
+const handleUserInput = () => {
+  return "data"
+}
+
+setupInput(handleUserInput())
